0597ac3f9a2ce5fa801eb79578dcfe1d4f11484f
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-03-31 20:08:17 +0200

    Update NauticalDart.cs

--- Content/Items/Ammo/NauticalDart.cs
+++ Content/Items/Ammo/NauticalDart.cs
@@ -17,7 +17,7 @@ namespace CalRemix.Content.Items.Ammo
 
         public override void SetDefaults()
         {
-            Item.damage = 5;
+            Item.damage = 12;
             Item.DamageType = DamageClass.Ranged;
             Item.width = 8;
             Item.height = 8;
