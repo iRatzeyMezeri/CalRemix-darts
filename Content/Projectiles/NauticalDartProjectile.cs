c3e9197e78da438a2333b0e36562ad795168f4bd
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-03-31 20:07:07 +0200

    Create NauticalDartProjectile.cs

---
+++ Content/Projectiles/NauticalDartProjectile.cs
@@ -0,0 +1,39 @@
+using CalamityMod.Buffs.StatDebuffs;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Projectiles
+{
+    public class NauticalDartProjectile : ModProjectile
+    {
+        public override string Texture => "CalRemix/Content/Items/Ammo/NauticalDart";
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("NauticalDart");
+        }
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.PoisonDartFriendly);
+            AIType = ProjectileID.PoisonDartFriendly;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() - MathHelper.PiOver2;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            int time = Projectile.ai[2] == 1 ? 600 : 120;
+            target.AddBuff(ModContent.BuffType<Eutrophication>(), time);
+        }
+
+        public override void OnHitPlayer(Player target, Player.HurtInfo info)
+        {
+            int time = Projectile.ai[2] == 1 ? 600 : 120;
+            target.AddBuff(ModContent.BuffType<Eutrophication>(), time);
+        }
+    }
+}
