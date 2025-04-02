241fe121c17929109b7603751cdd4ceaaafed6e2
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-04-02 16:10:45 +0200

    Create MysticDischarge.cs

---
+++ Content/Buffs/MysticDischarge.cs
@@ -0,0 +1,26 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Buffs
+{
+    public class MysticDischarge : ModBuff
+    {
+        public static float MultiplicativeDamageReductionPlayer = 0.9f;
+        public static float MultiplicativeDamageReductionEnemy = 0.9f;
+
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Mystic Discharge");
+            Description.SetDefault("We have no idea why you are taking damage, How MYSTical.");
+            Main.debuff[Type] = true;
+            Main.buffNoSave[Type] = true;
+        }
+
+        public override void Update(NPC npc, ref int buffIndex)
+        {
+            if (npc.Remix().mysticDischarge < npc.buffTime[buffIndex])
+                npc.Remix().mysticDischarge = npc.buffTime[buffIndex];
+            npc.DelBuff(buffIndex);
+            buffIndex--;
+        }
+    }
