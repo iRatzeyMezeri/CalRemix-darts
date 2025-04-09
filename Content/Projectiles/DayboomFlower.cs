7831cdeab96eb75de210e7c4bfc8d5802c7cd1a2
iRatzeyMezeri <125797032+iRatzeyMezeri@users.noreply.github.com>
2025-04-09 17:40:16 +0200

    Create DayboomFlower.cs

---
+++ Content/Projectiles/DayboomFlower.cs
@@ -0,0 +1,38 @@
+using CalamityMod.Buffs.StatDebuffs;
+using Microsoft.Xna.Framework;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalRemix.Content.Projectiles
+{
+    public class DayboomFlower : ModProjectile
+    {
+        public override void SetStaticDefaults()
+        {
+            DisplayName.SetDefault("Dayboom Flower");
+        }
+        public override void SetDefaults()
+        {
+            Projectile.CloneDefaults(ProjectileID.BulletFriendly);
+            AIType = ProjectileID.BulletFriendly;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation() - MathHelper.PiOver2;
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            int time = Projectile.ai[2] == 1 ? 600 : 600;
+            target.AddBuff(BuffID.Bleeding, time);
+        }
+
+        public override void OnHitPlayer(Player target, Player.HurtInfo info)
+        {
+            int time = Projectile.ai[2] == 1 ? 600 : 600;
+            target.AddBuff(BuffID.Bleeding, time);
+        }
+    }
+}
